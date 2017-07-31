4dd07d08bf56563c37cfc54d7f31c060b889c538
mvenditto <matteo.venditto@studio.unibo.it>
2017-07-31 17:17:01 +0200

    commit iniziale

---
+++ desktop/src/prova/repo/gdx/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package prova.repo.gdx.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import prova.repo.gdx.ProvaRepoGdx;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ProvaRepoGdx(), config);
+	}
+}
